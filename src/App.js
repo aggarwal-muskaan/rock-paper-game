ace8ab60d3de02d4fd75886e1ceb0a5aca8153fc
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-01-21 20:20:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import "./App.css";
+import Header from "./Header";
+import Pentagon from "./Pentagon";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Pentagon />
+    </div>
+  );
+}
+
+export default App;
