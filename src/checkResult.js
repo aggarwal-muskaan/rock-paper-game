3d29ae29c0996fa03ce6ce0b0128565625f242ad
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-01-27 20:27:34 +0530

    returning an array instead of string

--- src/checkResult.js
+++ src/checkResult.js
@@ -1,21 +1,21 @@
 /* eslint-disable */
-
 export default (user, comp) => {
-  console.log(user, comp);
   const difference = user - comp;
-  console.log(difference);
   switch (difference) {
     case -1:
     case -3:
     case 2:
     case 4:
-      return "YOU WIN";
+      return [0, "YOU WIN"];
+
     case -2:
     case -4:
     case 1:
     case 3:
-      return "YOU LOSE";
+      return [1, "YOU LOSE"];
+
     case 0:
-      return "It's TIE";
+      // currentScore = editScore(); //same score i.e. no increment & decrement
+      return [2, "It's TIE"];
   }
 };
